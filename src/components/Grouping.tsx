a032d0c5ba6d6f2478ead78740aa4dd283c14e12
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-10-10 11:51:14 +0530

    Grouping based on Priority Complete

--- src/components/Grouping.tsx
+++ src/components/Grouping.tsx
@@ -1,6 +1,8 @@
-import { status } from "../constants/utils";
+import { priority, status } from "../constants/utils";
 import { Data } from "../store/DataContext";
 import Card from "./Card";
+
+type Key = "No priority" | "Low" | "Medium" | "High" | "Urgent";
 
 export default function Grouping({
   grouping,
@@ -58,14 +60,50 @@ export default function Grouping({
       )}
 
       {grouping === "priority" && (
-        <>
-          <section style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-            <h4></h4>
-          </section>
-          <section></section>
-          <section></section>
-          <section></section>
-        </>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            width: "100vw",
+          }}
+        >
+          {Object.keys(priority).map((key, idx) => (
+            <div key={idx}>
+              <section
+                style={{ display: "flex", flexDirection: "column", gap: 8 }}
+              >
+                <p
+                  style={{
+                    fontFamily: "sans-serif",
+                    fontSize: 17,
+                  }}
+                >
+                  {key}{" "}
+                  {data?.tickets.reduce(
+                    (acc, ticket) =>
+                      ticket.priority === priority[key as Key] ? acc + 1 : acc,
+                    0
+                  )}
+                </p>
+                {data?.tickets.map(
+                  (ticket, idx) =>
+                    priority[key as Key] === ticket.priority && (
+                      <Card
+                        key={idx}
+                        id={ticket.id}
+                        title={ticket.title}
+                        priority={ticket.priority}
+                        status={ticket.status}
+                        tag={ticket.tag}
+                        userId={ticket.userId}
+                      />
+                    )
+                )}
+              </section>
+            </div>
+          ))}
+        </div>
       )}
 
       {grouping === "user" && (
--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,1 +1,14 @@
 export const status = ["Backlog", "Todo", "In progress", "Done", "Canceled"];
+export const priority: {
+  "No priority": 0;
+  Low: 1;
+  Medium: 2;
+  High: 3;
+  Urgent: 4;
+} = {
+  "No priority": 0,
+  Low: 1,
+  Medium: 2,
+  High: 3,
+  Urgent: 4,
+};
